25775190e727db08290a6eb71ed0e01e4e000df8
henning <henning.wagner@thyssenkrupp.com>
2026-06-20 10:52:19 +0200

    new init script

--- backend/tests/run_object_graph_2.py
+++ backend/tests/run_object_graph_2.py
@@ -1,31 +1,65 @@
 import os
 from dotenv import load_dotenv
 
-from backend.object_graph_runtime.graph_classes import CaseGraph, LegalState
+from backend.object_graph_runtime.graph_classes import CaseGraph, LegalState, Actor, ActorStatus, LegalNode, Case, utc_now
 from backend.expansion_engine.exapnsion_engine import ExpansionEngine
 from backend.llm_interface.llm_interface import MockLLMProvider
+from utils.utils import get_frontend_dir
 import json
 
 if __name__ == "__main__":
 
+    print("Start creating new graph")
     load_dotenv(override=True)
     openai_api_key = os.getenv('OPENAI_API_KEY')
 
     graph = CaseGraph()
 
 
-    state = LegalState(
-        phase ="Payment reminder sent",
-        legal_issue="Debtor has not paid the invoice after a reminder was sent.",
-        status="Overdue",
+    tim = Actor(id = '123', name='tim', role='plaintiff')
+    andi = Actor(id='1234', name='andi', role='debtor')
+
+    graph.actors =  {'tim' : tim, 'andi' : andi}
+    graph.case = Case(
+        id='7777',
+        title='my_case',
+        created_at=utc_now()
     )
 
-    # Initial node
-    start = graph.add_node(
-        title="Invoice Overdue",
-        state=state,
-        summary="Invoice overdue after reminder."
+    status_tim = ActorStatus(actor=tim,
+                             paid=0,
+                             received=0,
     )
+    status_andi = ActorStatus(actor=andi,
+                             paid=0,
+                             received=0,
+                             )
+
+    state = LegalState(
+        start_time= '2026-04-19T13:00:00',
+        end_time='2026-04-27T13:00:00',
+        legal_issue="Debtor has not paid the invoice after a reminder was sent.",
+        description="Debtor has not paid the invoice after a reminder was sent.",
+        final_state=False,
+        actors_status=[status_tim, status_andi],
+        legal_references=[],
+        artifacts=[],
+        deadlines=[]
+    )
+
+    init_node = LegalNode(
+        id ='12344',
+        case_id=graph.case.id,
+        incoming=[],
+        outgoing=[],
+        title='Debtor failed to pay',
+        state=state,
+        summary='Debtor failed to pay',
+    )
+
+
+    # Initial node
+    start = graph.add_node_obj(init_node)
 
     # Expansion engine
     llm = MockLLMProvider(key=openai_api_key)
@@ -33,10 +67,11 @@ if __name__ == "__main__":
     engine = ExpansionEngine(graph, llm)
 
     # Expand node
+    print("Creating new node")
     brach_node = engine.expand_node(start.id)
 
     print("Created node")
 
-
+    graph.to_json(os.path.join(get_frontend_dir(), 'src/data/graph.json'))
     print("\nSerialized graph:\n")
-    print(json.dumps(graph.to_dict(), indent=2))
+    #print(json.dumps(graph.to_dict(), indent=2))
